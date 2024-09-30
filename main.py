e73fecfc5fa415f2b8e22b5f3d25b98b2bd0f8ec
cristinadeviza <deviza.cristina.98@gmail.com>
2024-09-30 19:20:58 +0200

    6 Version

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 
-from flask import Flask, render_template,request,redirect,url_for
-import json
+# from flask import Flask, render_template,request,redirect,url_for
+# import json
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-dictionary = {}
+# dictionary = {}
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-@app.route("/redirect/<destination>", methods=["GET", "POST"])
-def google_redirect(destination):    
-    destinations = {
-        "github": "https://github.com/cristinadeviza/Huddle",        
-        "demo": "https://cristina-huddles.netlify.app/"
-        }    
-    if destination in destinations:       
-        return redirect(destinations[destination])    
-    else:        
-        return "Invalid destination", 400
+# @app.route("/redirect/<destination>", methods=["GET", "POST"])
+# def google_redirect(destination):    
+#     destinations = {
+#         "github": "https://github.com/cristinadeviza/Huddle",        
+#         "demo": "https://cristina-huddles.netlify.app/"
+#         }    
+#     if destination in destinations:       
+#         return redirect(destinations[destination])    
+#     else:        
+#         return "Invalid destination", 400
     
 
-@app.route("/projects")
-def projects():    
-    return redirect(url_for('index', _anchor="projects"))
-@app.route("/contacts")
-def contacts():    
-    return redirect(url_for('index', _anchor="contacts"))
+# @app.route("/projects")
+# def projects():    
+#     return redirect(url_for('index', _anchor="projects"))
+# @app.route("/contacts")
+# def contacts():    
+#     return redirect(url_for('index', _anchor="contacts"))
 
 
-@app.route('/linkedin')
-def linkedin():
-    return redirect("https://www.linkedin.com/in/cristina-deviza-704375271/", code=302)
+# @app.route('/linkedin')
+# def linkedin():
+#     return redirect("https://www.linkedin.com/in/cristina-deviza-704375271/", code=302)
 
-@app.route('/github')
-def github():
-    return redirect("https://github.com/cristinadeviza", code=302)
+# @app.route('/github')
+# def github():
+#     return redirect("https://github.com/cristinadeviza", code=302)
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
